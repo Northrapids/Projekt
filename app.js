f93a73c49683f31951a8ab2ec6355bfb5cff5a57
Northrapids <nordfors.fredrik@gmail.com>
2023-01-10 21:31:03 +0100

    final adjustments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// phone menu bar function
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar_menu');
 
@@ -7,38 +8,70 @@ menu.addEventListener('click', function() {
 });
 
 
-// thank you
-const btn = document.querySelector("#my-btn");
-const container = document.querySelector("#container");
+// thank you message apperas when send button is pressed
+// const btn = document.querySelector("#mybtn");
+// const container = document.querySelector("#container");
 
-btn.addEventListener("click", function (e) {
-    if (container.style.display === "block") {
-        container.style.display = "none";
-        e.preventDefault();
-    } else {
-        container.style.display = "block";
-        e.preventDefault();
-    }
-});
+// btn.addEventListener("click", function (e) {
+//     if (container.style.display === "block") {
+//         container.style.display = "none";
+//         e.preventDefault();
+//     } else {
+//         container.style.display = "block";
+//         e.preventDefault();
+//     }
+// });
 
-
-/* TOP BUTTON FUNCTION */
-// Get the button:
-let myBtn = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    myBtn.style.display = "block";
-  } else {
-    myBtn.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
+// scroll to top function when to top button is pressed
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
 }
+
+
+// const textEl = document.getElementById("colorMe");
+// const colorButtonEl = document.getElementById("colorButton");
+
+// function addClass(Event) {
+//     Event.preventDefault();
+//     e.stopImmediatePropagation()
+//     textEl.classList.toggle("changeColor");
+// }
+
+// colorButtonEl.addEventListener('click', addClass);
+
+
+// const buttonEl = document.querySelector('.button')
+
+// const contactformEl = document.querySelector(".contactform")
+
+// function messagetext(e) {
+//     e.preventDefault()
+//     contactformEl.innerHTML = "Your Message Successfully Sent!"
+// }
+
+// buttonEl.addEventListener('click', messagetext) 
+
+
+// test
+
+// document.getElementById("demo").innerHTML = "<font></font>;
+
+/* SEND A MESSAGE WHEN CLICK ON SUBMIT */ 
+
+const buttonEl = document.querySelector('.button');
+
+const contactformEl = document.querySelector(".contactform");
+// const hide2El = document.querySelector("hide2")
+
+function messagetext(e) {
+    e.preventDefault();
+    document.body.style.fontSize = '4rem'
+    document.body.style.fontFamily = 'Lato'
+
+    contactformEl.innerHTML = "Thank You For Your Message!";
+    // fontsize.innerHTML = 5rem;
+    // contactformEl.innerHTML = (".hide2");
+}
+
+buttonEl.addEventListener('click', messagetext); 
